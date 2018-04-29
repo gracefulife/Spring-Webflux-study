38c88a9a86df47fe30367a628c90764583cf0d72
Kyungman Kim <gracefulife@naver.com>
2018-04-29 15:08:20 +0900

    setup integration test

--- user/src/test/java/com/example/research/user/UserApplicationTests.java
+++ user/src/test/java/com/example/research/user/UserApplicationTests.java
@@ -2,15 +2,27 @@ package com.example.research.user;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserApplicationTests {
+  @Autowired WebTestClient webClient;
 
   @Test
-  public void contextLoads() {
+  public void testFindAll() {
+    this.webClient.get().uri("/").accept(MediaType.APPLICATION_JSON).exchange()
+        .expectStatus().isOk();
   }
 
+  @Test
+  public void testActuatorStatus() {
+    this.webClient.get().uri("/actuator").accept(MediaType.APPLICATION_JSON)
+        .exchange().expectStatus().isOk();
+  }
 }
