3e9bbb408ac72635f4d4984fa3cc4de6cbe78b38
gracefulife <gracefulife@naver.com>
2018-11-10 15:58:39 +0900

    init messenger project

---
+++ messenger/src/main/java/com/example/research/messenger/MessengerApplication.java
@@ -0,0 +1,12 @@
+package com.example.research.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MessengerApplication.class, args);
+  }
+}
